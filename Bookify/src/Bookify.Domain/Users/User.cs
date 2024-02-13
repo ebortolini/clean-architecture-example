2544fee15d63eaa7c830197613ff190c0640c3d3
Eduardo Bortolini <ebortolini@gmail.com>
2024-02-13 16:18:19 -0300

    Add User in Domain
     - User a factory to create users

---
+++ Bookify/src/Bookify.Domain/Users/User.cs
@@ -0,0 +1,25 @@
+﻿using Bookify.Domain.Abstratcions;
+
+namespace Bookify.Domain.Users
+{
+    public sealed class User : Entity
+    {
+        private User(Guid id,
+                     FirstName firstName,
+                     LastName lastName,
+                     Email email) : base(id) { }
+
+        public FirstName FirstName { get; private set; }
+
+        public LastName LastName { get; private set; }
+
+        public Email Email { get; private set; }
+
+        public static User Create(FirstName firstName, LastName lastName, Email email)
+        {
+            var user = new User(Guid.NewGuid(), firstName, lastName, email);
+
+            return user;
+        }
+    }
+}
