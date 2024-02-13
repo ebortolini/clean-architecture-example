518850f71b6831f24445bf6fd863b468899a9a81
Eduardo Bortolini <ebortolini@gmail.com>
2024-02-13 16:04:57 -0300

    Add entities

---
+++ Bookify/src/Bookify.Domain/Apartments/Money.cs
@@ -0,0 +1,15 @@
+﻿namespace Bookify.Domain.Apartments
+{
+    public record Money(decimal Amout, Currency Currency)
+    {
+        public static Money operator +(Money first, Money second)
+        {
+            if (first.Currency != second.Currency)
+                throw new InvalidOperationException("Currencies have to be equal");
+
+            return new Money(first.Amout + second.Amout, first.Currency);
+        }
+
+        public static Money Zero() => new Money(0, Currency.None);
+    }
+}
