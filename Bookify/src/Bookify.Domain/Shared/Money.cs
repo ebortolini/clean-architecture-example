eb4ee3d18119c35b7a39ea37b706e86f1f4d52c0
Eduardo Bortolini <ebortolini@gmail.com>
2024-02-13 23:34:15 -0300

    Add Booking Entity
     - Some small refactorings moving common code to a shared folder

--- Bookify/src/Bookify.Domain/Shared/Money.cs
+++ Bookify/src/Bookify.Domain/Shared/Money.cs
@@ -1,15 +1,19 @@
-﻿namespace Bookify.Domain.Apartments
+﻿namespace Bookify.Domain.Shared
 {
-    public record Money(decimal Amout, Currency Currency)
+    public record Money(decimal Amount, Currency Currency)
     {
         public static Money operator +(Money first, Money second)
         {
             if (first.Currency != second.Currency)
                 throw new InvalidOperationException("Currencies have to be equal");
 
-            return new Money(first.Amout + second.Amout, first.Currency);
+            return new Money(first.Amount + second.Amount, first.Currency);
         }
 
         public static Money Zero() => new Money(0, Currency.None);
+
+        public static Money Zero(Currency currency) => new(0, currency);
+
+        public bool IsZero() => this == Zero(Currency);
     }
 }
