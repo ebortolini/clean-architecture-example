61079e756d95dc0de9c1e95381e05c4f043fef73
Eduardo Bortolini <ebortolini@gmail.com>
2024-02-14 23:35:18 -0300

    Add Review Entity

---
+++ Bookify/src/Bookify.Domain/Reviews/Events/ReviewCreatedDomainEvent.cs
@@ -0,0 +1,6 @@
+﻿using Bookify.Domain.Abstratcions;
+
+namespace Bookify.Domain.Reviews.Events
+{
+    public sealed record ReviewCreatedDomainEvent(Guid ReviewId) : IDomainEvent;
+}
---
+++ Bookify/src/Bookify.Domain/Reviews/ReviewErrors.cs
@@ -0,0 +1,11 @@
+﻿using Bookify.Domain.Abstratcions;
+
+namespace Bookify.Domain.Reviews
+{
+    public static class ReviewErrors
+    {
+        public static readonly Error NotEligible = new(
+            "Review.NotEligible",
+            "The review is not eligible because the booking is not yet completed");
+    }
+}
