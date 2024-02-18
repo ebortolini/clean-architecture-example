7890cb238fb1ad181b92c63bf00342efe6dd053e
Eduardo Bortolini <ebortolini@gmail.com>
2024-02-18 00:02:49 -0300

    Implement Reserve Booking

---
+++ Bookify/src/Bookify.Domain/Apartments/ApartmentErrors.cs
@@ -0,0 +1,11 @@
+﻿using Bookify.Domain.Abstratcions;
+
+namespace Bookify.Domain.Apartments
+{
+    public static class ApartmentErrors
+    {
+        public static readonly Error NotFound = new(
+           "Apartment.NotFound",
+           "The apartment with the specified identifier was not found");
+    }
+}
