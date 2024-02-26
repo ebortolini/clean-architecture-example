3221a39abcdc71a26dca3f375344bc9476633b87
Eduardo Bortolini <ebortolini@gmail.com>
2024-02-25 23:24:56 -0300

    Missing files from previous commit

--- Bookify/src/Bookify.Application/Apartments/SearchApartments/ApartmentResponse.cs
+++ Bookify/src/Bookify.Application/Apartments/SearchApartments/ApartmentResponse.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Bookify.Application.Apartments.SearchApartments
+﻿namespace Bookify.Application.Apartments.SearchApartments
 {
     public sealed class ApartmentResponse
     {
