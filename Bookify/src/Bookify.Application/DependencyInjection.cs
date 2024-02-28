853036e4117e3f661786da1bb04ba69c284bef41
Eduardo Bortolini <ebortolini@gmail.com>
2024-02-27 22:37:29 -0300

    Creating Validation Pipeline

--- Bookify/src/Bookify.Application/DependencyInjection.cs
+++ Bookify/src/Bookify.Application/DependencyInjection.cs
@@ -1,5 +1,6 @@
 ﻿using Bookify.Application.Abstractions.Behaviors;
 using Bookify.Domain.Bookings;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Bookify.Application
@@ -12,7 +13,10 @@ namespace Bookify.Application
             {
                 configuration.RegisterServicesFromAssemblies(typeof(DependencyInjection).Assembly);
                 configuration.AddOpenBehavior(typeof(LoggingBehavior<,>));
+                configuration.AddBehavior(typeof(ValidationBehavior<,>));
             });
+
+            services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
 
             services.AddTransient<PricingService>();
 
