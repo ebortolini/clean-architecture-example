94017222173d64c1244de91666e1a1c0fdc7d797
Eduardo Bortolini <ebortolini@gmail.com>
2024-03-29 23:10:22 -0300

    Add custom SQL Health Check

--- Bookify/src/Bookify.Api/Program.cs
+++ Bookify/src/Bookify.Api/Program.cs
@@ -1,6 +1,11 @@
 using Bookify.Api.Extensions;
 using Bookify.Application;
+using Bookify.Application.Abstractions.Data;
 using Bookify.Infrastructure;
+using Dapper;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,10 +16,14 @@ builder.Host.UseSerilog((contex, configuration) =>
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddApplication();
+
 builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddHealthChecks().AddCheck<CustomSqsHealthCheck>("custom-sql");
 
 var app = builder.Build();
 
@@ -43,4 +52,30 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.MapHealthChecks("health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse,
+});
+
 app.Run();
+
+public class CustomSqsHealthCheck(ISqlConnectionFactory sqlConnectionFactory) : IHealthCheck
+{
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, 
+        CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            using var connection = sqlConnectionFactory.CreateConnection();
+
+            await connection.ExecuteScalarAsync("SELECT 1;");
+
+            return HealthCheckResult.Healthy();
+        }
+        catch (Exception e)
+        {
+            return HealthCheckResult.Unhealthy(exception: e);
+        }
+        
+    }
+}
